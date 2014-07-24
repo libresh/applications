37c6aa01ca9ce4f6b22bd4394b944c7466cea242
Michiel de Jong <michiel@unhosted.org>
2014-07-24 12:50:39 +0200

    take michiel-data folder from /root/

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@ sudo docker stop resite
 sudo docker rm resite
 sudo docker stop bouncer
 sudo docker rm bouncer
-sudo docker run -d -v /home/michiel/michiel-data:/data --name resite michielbdejong/resite
-sudo docker run -d -v /home/michiel:/home/michiel --name bouncer --link resite:resite -p 80:80 -p 443:443 michielbdejong/3pp-bouncer
+sudo docker run -d -v /root/michiel-data:/data --name resite michielbdejong/resite
+sudo docker run -d -v /root/michiel-data:/home/michiel/michiel-data --name bouncer --link resite:resite -p 80:80 -p 443:443 michielbdejong/3pp-bouncer
 sudo docker ps
