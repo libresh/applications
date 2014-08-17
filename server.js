c13bb71ef12fbe56b23ee4f7e85af87a87b7fd89
Michiel de Jong <michiel@unhosted.org>
2014-08-17 19:48:59 +0200

    make bouncer more ready for multiple domains

--- server.js
+++ server.js
@@ -5,24 +5,29 @@ var spdy = require('spdy'),
     config = JSON.parse(fs.readFileSync('/data/bouncer/3pp.json'));
 
 b64decode = function(encoded) {
-    return new Buffer(encoded || '', 'base64').toString('utf8');
+  return new Buffer(encoded || '', 'base64').toString('utf8');
 };
 
-var keys = {
-    "key": fs.readFileSync('/data/bouncer/cert/'+config.cert+'/tls.key'),
-    "cert": fs.readFileSync('/data/bouncer/cert/'+config.cert+'/tls.cert'),
-    "chain": fs.readFileSync('/data/bouncer/cert/'+config.cert+'/chain.pem')
-};
-var loadedKeys = crypto.createCredentials(keys).context;
+var keys = {}, loadedKeys = {};
 
-var config = {
-    ssl: keys,
-};
+for(domain in config.certs) {
+  keys[domain] = {
+    "key": fs.readFileSync('/data/bouncer/cert/'+config.certs[domain]+'/tls.key'),
+    "cert": fs.readFileSync('/data/bouncer/cert/'+config.certs[domain]+'/tls.cert'),
+    "chain": fs.readFileSync('/data/bouncer/cert/'+config.certs[domain]+'/chain.pem')
+  };
+  loadedKeys[domain] = crypto.createCredentials(keys).context;
+}
 
-config.ssl.SNICallback = function(hostname, cb) {
-    if (cb) // 0.11.5 and later
-        return cb(null, loadedKeys);
-    return loadedKeys; // 0.11.4 and earlier;
+var spdyConfig = {
+  ssl: keys['default'],
+};
+console.log(config, keys, spdyConfig);
+spdyConfig.ssl.SNICallback = function(hostname, cb) {
+  if (cb) {// 0.11.5 and later
+    return cb(null, loadedKeys[hostname]);
+  }
+  return loadedKeys[hostname]; // 0.11.4 and earlier;
 };
 
 var http = require('http');
@@ -30,38 +35,51 @@ var http = require('http');
 var httpProxy = require('http-proxy');
 var proxy = httpProxy.createProxyServer({/*options*/});
 
-spdy.createServer(config.ssl,
-  function(req, res) {
-    console.log('proxying web '+req.url);
-    try {
-      proxy.web(req, res, { target: 'http://' + process.env.RESITE_PORT_80_TCP_ADDR });
-    } catch(e) {
-      console.log('proxy fail', e);
-      res.writeHead(500);
-      req.end();
-    }
-    console.log('proxied web '+req.url);
-  }
-).listen(443);
+function makeTarget(route) {
+  var envVar = route.app.toUpperCase() + '_PORT_' + route.port + '_TCP_ADDR';
+  return 'http://' + process.env[envVar] + ':' + route.port
+}
 
-spdy.createServer(config.ssl,
-  function(req, res) {
-    console.log('proxying 7678 '+req.url);
-    try {
-      proxy.web(req, res, { target: 'http://' + process.env.RESITE_PORT_7678_TCP_ADDR + ':7678' });
-    } catch(e) {
-      console.log('proxy fail', e);
-      res.writeHead(500);
-      req.end();
+function doProxy(req, res, port) {
+  console.log('proxying '+port+' '+req.url);
+  try {
+    for (domain in config.routes[port]) {
+      if (req.headers.host === domain) {
+        target = makeTarget(config.routes[port][domain]);
+        proxy.web(req, res, { target: target });
+        console.log('proxied '+port+' '+req.url+' to '+target);
+        return;
+      }
     }
-    console.log('proxied 7678 '+req.url);
+    target = makeTarget(config.routes[port][domain]);
+    proxy.web(req, res, { target: target }) ;
+    console.log('proxied '+port+' '+req.url+' to default '+target);
+  } catch(e) {
+    console.log('proxy fail', e);
+    res.writeHead(500);
+    req.end();
   }
-).listen(7678);
+  console.log('proxied '+port+' '+req.url);
+}
+
+function proxyPort(port) {
+  spdy.createServer(spdyConfig.ssl, function(req, res) {
+    return doProxy(req, res, port);
+  }).listen(port);
+}
+
+//...
+proxyPort(443);
+proxyPort(7678);
 
 http.createServer(function(req, res) {
   console.log('request port 80', req.url);
-  res.writeHead(302, {
-    Location: 'https://michielbdejong.com'+req.url //todo: read this from the host header
-  });
-  res.end();
+  if(config.redirect[req.headers.host]) {
+    res.writeHead(302, {
+      Location: config.redirect[req.headers.host] + req.url
+    });
+    res.end();
+  } else {
+    doProxy(req, res, 80);
+  }
 }).listen(80);
