4c6105a0c7436c1b196d50827833c59fdfa08483
Michiel de Jong <michiel@unhosted.org>
2014-08-20 15:08:13 +0200

    use /data/server-wide and /data/per-user in run.sh

--- run.sh
+++ run.sh
@@ -4,23 +4,23 @@ docker build -t indiehosters/mail server-wide/mail/
 
 docker stop resitedefault
 docker rm resitedefault
-docker run -d -v /data/default/resite:/data/resite --name resitedefault indiehosters/resite
+docker run -d -v /data/per-user/default/resite:/data/resite --name resitedefault indiehosters/resite
 docker logs resitedefault
 
 docker stop resitemichiel
 docker rm resitemichiel
-docker run -d -v /data/michiel/resite:/data/resite --name resitemichiel indiehosters/resite
+docker run -d -v /data/per-user/michielbdejong.com/resite:/data/resite --name resitemichiel indiehosters/resite
 docker logs resitemichiel
 
 #docker stop resitejoebloggs
 #docker rm resitejoebloggs
-#docker run -d -v /data/joebloggs/resite:/data/resite --name resitejoebloggs indiehosters/resite
+#docker run -d -v /data/per-user/joebloggs.com/resite:/data/resite --name resitejoebloggs indiehosters/resite
 #docker logs resitejoebloggs
 
 docker stop bouncer
 docker rm bouncer
 #... --link resitejoebloggs:resitejoebloggs ...
-docker run -d -v /data/default/bouncer:/data/bouncer --name bouncer --link resitedefault:resitedefault --link resitemichiel:resitemichiel -p 80:80 -p 443:443 -p 7678:7678 indiehosters/bouncer
+docker run -d -v /data/server-wide/bouncer:/data/bouncer --name bouncer --link resitedefault:resitedefault --link resitemichiel:resitemichiel -p 80:80 -p 443:443 -p 7678:7678 indiehosters/bouncer
 docker logs bouncer
 
 docker ps
