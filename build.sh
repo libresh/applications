dcdc77aba52556b1dd973e447450b8dc7aef449d
Michiel de Jong <michiel@unhosted.org>
2014-10-10 17:44:10 +0100

    add anchorcms to build

--- build.sh
+++ build.sh
@@ -11,6 +11,10 @@ git submodule update
   git checkout add-Dockerfile
   cd ../..
 
+  cd per-user/anchorcms
+  git checkout add-Dockerfile
+  cd ../..
+
 for folder in server-wide per-user; do
   for image in $(find $folder/* -type d | cut -d "/" -f 2); do
     sudo docker build -t indiehosters/$image $folder/$image/;
