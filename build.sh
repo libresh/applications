755845bd1a88b7bc05f04d26e574bf70c0fad1bd
Michiel de Jong <michiel@unhosted.org>
2014-10-10 15:21:40 +0100

    add anchorcms and owncloud

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
 git pull --rebase
+git submodule update
+
+  cd server-wide/yunohost
+  git checkout add-Dockerfile
+  cd ../..
+
+  cd per-user/idno
+  git checkout add-Dockerfile
+  cd ../..
 
 for folder in server-wide per-user; do
   for image in $(find $folder/* -type d | cut -d "/" -f 2); do
