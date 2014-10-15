c0b7c084ff8872d4491af99942241de9ba037b13
Michiel de Jong <michiel@indiehosters.net>
2014-10-15 17:19:59 +0200

    try to mix&match a postfix image together

---
+++ mail/postfix/assets/install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#judgement
+if [[ -a /etc/supervisor/conf.d/supervisord.conf ]]; then
+  exit 0
+fi
+
+#supervisor
+cat > /etc/supervisor/conf.d/supervisord.conf <<EOF
+[supervisord]
+nodaemon=true
+
+[program:postfix]
+command=/opt/postfix.sh
+
+[program:rsyslog]
+command=/usr/sbin/rsyslogd -n -c3
+EOF
+
+############
+# postfix
+############
+cat >> /opt/postfix.sh <<EOF
+#!/bin/bash
+service postfix start
+tail -f /var/log/mail.log
+EOF
+chmod +x /opt/postfix.sh
