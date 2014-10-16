a60cca2faddbcac8c6240a81caaf2ba7c94f47bd
Michiel de Jong <michiel@indiehosters.net>
2014-10-16 19:47:40 +0200

    adding forwards

--- mail/postfix/assets/install.sh
+++ mail/postfix/assets/install.sh
@@ -28,7 +28,18 @@ tail -f /var/log/mail.log
 EOF
 chmod +x /opt/postfix.sh
 
-# Configure email domain on first run:
-echo ${HOSTNAME} > /etc/mailname
-/usr/sbin/postconf -e "myhostname=${HOSTNAME}"
-/usr/sbin/postconf -e "mydestination=${HOSTNAME}"
+# put the same FQDN in /data/hostname and in reverse DNS
+# for the public IP address on which this server will be
+# receiving smtp traffic.
+cp /data/hostname /etc/mailname
+/usr/sbin/postconf -e "myhostname=`cat /data/hostname`"
+
+# put all relevant domains in /data/destinations.
+/usr/sbin/postconf -e "mydestination=`cat /data/destinations`"
+
+#put your forwarding addresses in /data/forwards.
+echo "virtual_maps = hash:/etc/postfix/virtual, regexp:/etc/postfix/virtual-regexp" >> /etc/postfix/main.cf
+cp /data/forwards /etc/postfix/virtual-regexp
+postmap /etc/postfix/virtual-regexp
+touch /etc/postfix/virtual
+postmap /etc/postfix/virtual
