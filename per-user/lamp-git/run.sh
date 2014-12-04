5c3e3fa3dc21643e500f3e967bb5ec0f02963539
Michiel de Jong <michiel@unhosted.org>
2014-12-04 16:14:40 +0100

    try to make Known uploads work on lamp-git image

--- per-user/lamp-git/run.sh
+++ per-user/lamp-git/run.sh
@@ -3,6 +3,9 @@
 git config --global user.email "backup@IndieHosters"
 git config --global user.name "IndieHosters backup"
 
+chown root:www-data /data/uploads
+chmow g+rwx /data/uploads
+
 exec mysqld_safe &
 exec cron &
 sleep 5
