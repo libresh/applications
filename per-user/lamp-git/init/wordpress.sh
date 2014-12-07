bcee60352fcbb57a8ebf9ca897082d0ee9cdf6f7
root @ l1 <root@l1>
2014-12-07 13:14:12 +0000

    update list of IndieWeb plugins for WordPress

--- per-user/lamp-git/init/wordpress.sh
+++ per-user/lamp-git/init/wordpress.sh
@@ -10,8 +10,47 @@ php /root/wp-cli.phar --path="/data/www-content" --allow-root db create
 PWD=`pwgen 40 1`
 php /root/wp-cli.phar --path="/data/www-content" --allow-root core install \
 	--url="$1" --title="$2" --admin_user="$3" --admin_password="$PWD" --admin_email="$4"
+
+#TODO: Debug if/why this is necessary:
+php /root/wp-cli.phar --path="/data/www-content" --allow-root user update $3 --user_pass="$PWD" 
+
+echo Installing the IndieWeb plugins...
+php /root/wp-cli.phar --path="/data/www-content" --allow-root plugin delete akismet
+php /root/wp-cli.phar --path="/data/www-content" --allow-root plugin delete hello
+
 php /root/wp-cli.phar --path="/data/www-content" --allow-root plugin install wordpress-https
 php /root/wp-cli.phar --path="/data/www-content" --allow-root plugin activate wordpress-https
+
+php /root/wp-cli.phar --path="/data/www-content" --allow-root plugin install indieauth
+php /root/wp-cli.phar --path="/data/www-content" --allow-root plugin activate indieauth
+
+php /root/wp-cli.phar --path="/data/www-content" --allow-root plugin install semantic-linkbacks
+php /root/wp-cli.phar --path="/data/www-content" --allow-root plugin activate semantic-linkbacks
+
+php /root/wp-cli.phar --path="/data/www-content" --allow-root plugin install webmention
+php /root/wp-cli.phar --path="/data/www-content" --allow-root plugin activate webmention
+
+php /root/wp-cli.phar --path="/data/www-content" --allow-root plugin install hum
+php /root/wp-cli.phar --path="/data/www-content" --allow-root plugin activate hum
+
+php /root/wp-cli.phar --path="/data/www-content" --allow-root plugin install https://github.com/pfefferle/wordpress-indieweb-press-this/archive/master.zip
+php /root/wp-cli.phar --path="/data/www-content" --allow-root plugin activate wordpress-indieweb-press-this-master
+
+php /root/wp-cli.phar --path="/data/www-content" --allow-root plugin install https://github.com/dshanske/indieweb-taxonomy/archive/master.zip
+php /root/wp-cli.phar --path="/data/www-content" --allow-root plugin activate indieweb-taxonomy-master
+
+php /root/wp-cli.phar --path="/data/www-content" --allow-root plugin install https://github.com/pfefferle/wordpress-webactions/archive/master.zip
+php /root/wp-cli.phar --path="/data/www-content" --allow-root plugin activate wordpress-webactions-master
+
+php /root/wp-cli.phar --path="/data/www-content" --allow-root plugin install https://github.com/pfefferle/wordpress-webmention-for-comments/archive/master.zip
+php /root/wp-cli.phar --path="/data/www-content" --allow-root plugin activate wordpress-webmention-for-comments-master
+
+php /root/wp-cli.phar --path="/data/www-content" --allow-root plugin install indieweb
+php /root/wp-cli.phar --path="/data/www-content" --allow-root plugin activate indieweb
+
+php /root/wp-cli.phar --path="/data/www-content" --allow-root theme install sempress
+php /root/wp-cli.phar --path="/data/www-content" --allow-root theme activate sempress
+
 echo "user: $3" > /data/login.txt
 echo "pass: $PWD" >> /data/login.txt
 echo "Done, login details saved to /data/login.txt"
