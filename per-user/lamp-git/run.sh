e78d92cdb7e2312aae90e55cd2f8a198d9e14863
Michiel de Jong <michiel@unhosted.org>
2014-12-07 08:06:38 +0100

    fix the way mysql and backups are started

--- per-user/lamp-git/run.sh
+++ per-user/lamp-git/run.sh
@@ -1,24 +1,40 @@
 #!/bin/bash
 
-git config --global user.email "backup@IndieHosters"
-git config --global user.name "IndieHosters backup"
-
-chown -R root:www-data /data/uploads
-chmod -R 770 /data/uploads
-
 exec mysqld_safe &
-exec cron &
-sleep 5
+for ((i=0;i<10;i++))
+do
+    DB_CONNECTABLE=$(mysql -e 'status' >/dev/null 2>&1; echo "$?")
+    if [[ DB_CONNECTABLE -eq 0 ]]; then
+        break
+    fi
+    echo "Waiting for database server..."
+    sleep 1
+done
+
+mkdir -p /data/uploads
+mkdir -p /data/www-content
+touch /data/dump.sql
+
+chown -R root:www-data /data
+chmod -R 770 /data/uploads
+chmod -R 750 /data/www-content
+
 mysql < /data/dump.sql
 source /etc/apache2/envvars
 exec apache2 &
+
 cd /data
-while (1); do
-  mysqldump --all-databases > dump.sql;
-  git add *;
-  git commit -am"backup `date`";
-  git status;
-  date;
-  echo "Next backup in one hour...";
-  sleep 3540;
+git init
+git config --local user.email "backup@IndieHosters"
+git config --local user.name "IndieHosters backup"
+git config --local push.default simple
+
+while true; do
+  mysqldump --all-databases > dump.sql
+  git add *
+  git commit -am"backup `date`"
+  git status
+  date
+  echo "Next backup in one hour..."
+  sleep 3540
 done
